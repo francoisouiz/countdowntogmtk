b879f827d84bf883d9dbab239b23399244959e8e
francoisouiz <fezingapan@up.edu.ph>
2026-07-26 01:22:29 +0800

    feat: add room collision and doors

--- scripts/door.gd
+++ scripts/door.gd
@@ -1,12 +1,15 @@
 extends Area2D
 
-@export var connected_room: String
-
 @export var direction: Vector2i
 
+var disabled: bool = false
 var locked: bool = false
 
+func _ready() -> void:
+	if disabled:
+		visible = false
+
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player") and !locked:
+	if body.is_in_group("player") and !locked and !disabled:
 		RoomChangeGlobal.activate = true
 		RoomChangeGlobal.player_pos = position + Vector2(direction)
