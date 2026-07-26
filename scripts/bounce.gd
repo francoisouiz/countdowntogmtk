112281169648dfca400f805058eb161bf40e2468
AlexanderPCristobal <b2025apcristobal@pshs.edu.ph>
2026-07-26 21:35:27 +0800

    feat: add upgrades

---
+++ scripts/bounce.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+func pickup(player: Node2D) -> void:
+	player.inven.append("bounce")
+	queue_free()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is Player:
+		pickup(body)
